5960669fa7027d11f610769698caa0b771ef08cf
Onkar Madli <onkarmadli@gmail.com>
2021-09-28 19:00:51 +0530

    Add base HTML and JS logic

---
+++ script.js
@@ -0,0 +1,26 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector('#output-box');
+
+
+function isBirthDateLucky(){
+  const date= dateOfBirth.value;
+  const sum = calculateSum(dateOfBirth.value);
+  if(sum%luckyNumber.value === 0){
+    outputBox.innerText = "Your birthday is Lucky!";
+  } else{
+    outputBox.innerText = "Your birthday is NOT Lucky!";
+  }
+}
+
+function calculateSum(birthDate) {
+  birthDate = birthDate.replaceAll("-","");
+  let sum = 0;
+  for(let digit=0;digit<birthDate.length;digit++){
+    sum += Number(birthDate.charAt(digit));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener('click',isBirthDateLucky);
